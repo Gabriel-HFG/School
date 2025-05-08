03405c3d46f7a2df81bb135a9e72a97972dadfda
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-05-08 16:03:30 +0000

    gggggggggg

--- powers.c
+++ powers.c
@@ -1,9 +1,21 @@
 
 #include <stdio.h>
     int main() {
-        int awnser_1, awnser_2, awnser_3;
+        int awnser_1, awnser_2, awnser_3, result, def;
         printf("Input 2 numbers first number will be the base and the second will be the power: ");
         scanf("%d%d", &awnser_1, &awnser_2); awnser_3 = awnser_1;
+        printf("1. While\n2. for loop\nInput:");
+        scanf("%d", &def);
 
+        if (def = 1) {
         if (awnser_2 <= 1) {printf("You cant power a number with this number %d", awnser_2);} else {
-        while (awnser_2 != 1) {awnser_1 = (awnser_1 * awnser_3); --awnser_2;} printf("The result is: %d\n\n", awnser_1);}}
+        while (awnser_2 != 1) {awnser_1 = (awnser_1 * awnser_3); --awnser_2;} printf("The result is: %d\n\n", awnser_1);}
+
+        } else if (def = 2) { 
+        for (int i = 1; i <= awnser_2; i++) {
+            result *= awnser_1; }
+        } else {
+            printf("Error");
+            return 0;
+        } 
+    }
